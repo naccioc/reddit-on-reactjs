b60a08d41fc2f443b4e7909e90e78ff9b27ae8a4
naccioc <naccioc@gmail.com>
2021-01-22 17:14:11 +0000

    Set material-ui bases for SSR/SSG

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,33 @@
+import Head from 'next/head'
 import { Provider } from 'react-redux'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
 import { useStore } from '../state/store'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   const store = useStore(pageProps.initialState)
+  const theme = createMuiTheme({
+    palette: {
+      type: 'dark'
+    }
+  })
 
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Provider>
+    </>
   )
 }
 
