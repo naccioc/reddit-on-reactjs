d5859074b281d860ceea464d7a41e1ad348575dc
naccioc <naccioc@gmail.com>
2021-02-08 13:59:13 +0000

    Pagination support

--- components/List/List.js
+++ components/List/List.js
@@ -1,4 +1,4 @@
-import { Divider, IconButton, ListSubheader } from '@material-ui/core';
+import { Button, Divider, IconButton, ListSubheader } from '@material-ui/core';
 import MuiList from '@material-ui/core/List';
 import ChevrontLeftIcon from '@material-ui/icons/ChevronLeft';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -7,7 +7,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import { closeDrawer } from '../../state/actions/drawer';
-import { deleteAll } from '../../state/actions/post';
+import { deleteAll, getPosts } from '../../state/actions/post';
 import ListItem from '../ListItem';
 import useListItemStyles from '../ListItem/ListItem.styles';
 import useStyles from './List.styles';
@@ -21,42 +21,59 @@ function List() {
   // Handlers
   const handleDeleteAll = () => dispatch(deleteAll());
   const handleCloseDrawer = () => dispatch(closeDrawer());
+  const handleLoadMore = (event) => {
+    const after = event.currentTarget.getAttribute('data-id');
+
+    dispatch(getPosts(10, after));
+  };
 
   return (
-    <MuiList
-      component="nav"
-      subheader={
-        <ListSubheader className={classes.listSubheader} component="h2">
-          Reddit Posts
-          <div className={classes.listSubheader_actions}>
-            <IconButton onClick={handleDeleteAll}>
-              <DeleteIcon />
-            </IconButton>
-            <IconButton onClick={handleCloseDrawer}>
-              <ChevrontLeftIcon />
-            </IconButton>
-          </div>
-        </ListSubheader>
-      }
-    >
-      <TransitionGroup>
-        {posts.map((post, index, array) => (
-          <CSSTransition
-            key={post.data.id}
-            timeout={500}
-            classNames={{
-              exit: listItemClasses.listItem_exit,
-              exitActive: listItemClasses.listItem_exit_active
-            }}
-          >
-            <Fragment>
-              <ListItem post={post} />
-              {array.length - 1 !== index && <Divider variant="inset" />}
-            </Fragment>
-          </CSSTransition>
-        ))}
-      </TransitionGroup>
-    </MuiList>
+    <Fragment>
+      <MuiList
+        component="nav"
+        subheader={
+          <ListSubheader className={classes.listSubheader} component="h2">
+            Reddit Posts
+            <div className={classes.listSubheader_actions}>
+              <IconButton onClick={handleDeleteAll}>
+                <DeleteIcon />
+              </IconButton>
+              <IconButton onClick={handleCloseDrawer}>
+                <ChevrontLeftIcon />
+              </IconButton>
+            </div>
+          </ListSubheader>
+        }
+      >
+        <TransitionGroup component={null}>
+          {posts.map((post, index, array) => (
+            <CSSTransition
+              key={post.data.id}
+              timeout={500}
+              classNames={{
+                exit: listItemClasses.listItem_exit,
+                exitActive: listItemClasses.listItem_exit_active
+              }}
+            >
+              <Fragment>
+                <ListItem post={post} />
+                {array.length - 1 !== index && <Divider variant="inset" />}
+              </Fragment>
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
+      </MuiList>
+      {posts.length !== 0 && posts.length < 50 && (
+        <Button
+          variant="contained"
+          color="primary"
+          data-id={posts[posts.length - 1].data.name}
+          onClick={handleLoadMore}
+        >
+          Load more
+        </Button>
+      )}
+    </Fragment>
   );
 }
 
